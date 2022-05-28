4723024cbf4222016f196e9ebc73e240d6af3e31
Phúc <quangphuc2409@gmail.com>
2022-05-28 10:48:40 +0700

    Hiện context menu khi nhấn giữ list view

--- Bai1/app/src/main/java/com/bai1/MainActivity.java
+++ Bai1/app/src/main/java/com/bai1/MainActivity.java
@@ -1,11 +1,18 @@
 package com.bai1;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView.AdapterContextMenuInfo;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
@@ -43,6 +50,8 @@ public class MainActivity extends AppCompatActivity implements IOnItemClickListe
             Intent intent = new Intent(this, AddContactActivity.class);
             startActivityForResult(intent, REQUEST_ADD);
         });
+
+        registerForContextMenu(listView);
     }
 
     private void updateAdapter() {
@@ -61,6 +70,26 @@ public class MainActivity extends AppCompatActivity implements IOnItemClickListe
             db.insert(contact);
             updateAdapter();
         }
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_context, menu);
+    }
+
+    @Override
+    public boolean onContextItemSelected(@NonNull MenuItem item) {
+        AdapterContextMenuInfo info = (AdapterContextMenuInfo) item.getMenuInfo();
+        int position = info.position;
+
+        if (item.getItemId() == R.id.itemDelete) {
+            Toast.makeText(this, position + "", Toast.LENGTH_SHORT).show();
+        }
+
+        return super.onContextItemSelected(item);
     }
 
     @Override
